85019faf464c741c226c133c90c90f11f7025d8a
YoungSun Shin <tlsdudtjs128@gmail.com>
2019-05-31 16:02:48 +0900

    #1 폼 디자인, 코드 일부분 완성

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -28,10 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.teaComboBox = new System.Windows.Forms.ComboBox();
             this.Soaking = new System.Windows.Forms.Button();
-            this.countTime = new System.Windows.Forms.Label();
+            this.selectTea = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.countTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // teaComboBox
@@ -41,6 +44,7 @@
             this.teaComboBox.Name = "teaComboBox";
             this.teaComboBox.Size = new System.Drawing.Size(246, 20);
             this.teaComboBox.TabIndex = 0;
+            this.teaComboBox.SelectedIndexChanged += new System.EventHandler(this.TeaComboBox_SelectedIndexChanged);
             // 
             // Soaking
             // 
@@ -50,15 +54,16 @@
             this.Soaking.TabIndex = 1;
             this.Soaking.Text = "담그기!";
             this.Soaking.UseVisualStyleBackColor = true;
+            this.Soaking.Click += new System.EventHandler(this.Soaking_Click);
             // 
-            // countTime
+            // selectTea
             // 
-            this.countTime.AutoSize = true;
-            this.countTime.Location = new System.Drawing.Point(12, 164);
-            this.countTime.Name = "countTime";
-            this.countTime.Size = new System.Drawing.Size(65, 12);
-            this.countTime.TabIndex = 2;
-            this.countTime.Text = "남은 시간: ";
+            this.selectTea.AutoSize = true;
+            this.selectTea.Location = new System.Drawing.Point(12, 164);
+            this.selectTea.Name = "selectTea";
+            this.selectTea.Size = new System.Drawing.Size(65, 12);
+            this.selectTea.TabIndex = 2;
+            this.selectTea.Text = "선택한 차: ";
             // 
             // label2
             // 
@@ -69,17 +74,33 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "3110_신영선";
             // 
+            // countTime
+            // 
+            this.countTime.AutoSize = true;
+            this.countTime.Location = new System.Drawing.Point(14, 190);
+            this.countTime.Name = "countTime";
+            this.countTime.Size = new System.Drawing.Size(57, 12);
+            this.countTime.TabIndex = 4;
+            this.countTime.Text = "남은 시간";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(271, 329);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.countTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.selectTea);
             this.Controls.Add(this.Soaking);
             this.Controls.Add(this.teaComboBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,8 +110,10 @@
 
         private System.Windows.Forms.ComboBox teaComboBox;
         private System.Windows.Forms.Button Soaking;
-        private System.Windows.Forms.Label countTime;
+        private System.Windows.Forms.Label selectTea;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label countTime;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -12,12 +12,66 @@ namespace TeaBagMaker
     public partial class Form1 : Form
     {
         // 초기 콤보박스 데이터 설정
-        string[] SList = new string[]{"홍차", "녹차", };
+        string[] teaList = new string[]{"홍차", "녹차", "루이보이스차", "국화차"};
         string orgStr = ""; // 선택 결과 저장
+
+        int CountOrgNum = 120;
 
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < teaList.Length; i++)
+            {
+                this.teaComboBox.Items.Add(teaList[i]);
+            }
+
+            if (teaComboBox.Items.Count > 0)
+            {
+                this.teaComboBox.SelectedIndex = 1;
+            }
+
+        }
+
+        private void TeaComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.teaComboBox.Text != "")
+            {
+                this.orgStr = selectTea.Text;
+                this.selectTea.Text = orgStr + this.teaComboBox.Text;
+            }
+
+        }
+
+        private void Soaking_Click(object sender, EventArgs e)
+        {
+            if (orgStr == "홍차") { CountOrgNum = 120; }
+            else if (orgStr == "녹차") { CountOrgNum = 180; }
+            else if (orgStr == "루이보이스차") { CountOrgNum = 300; }
+            else if (orgStr == "국화차") { CountOrgNum = 120; }
+            this.timer1.Enabled = true;
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            if (CountOrgNum < 1)
+            {
+                this.timer1.Enabled = false;
+                MessageBox.Show("티백을 건지세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.teaComboBox.Focus();
+            }
+            else
+            {
+                this.CountOrgNum--;
+                this.countTime.Text = Convert.ToString(this.CountOrgNum);
+            }
+
         }
     }
 }
